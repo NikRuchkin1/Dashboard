ae06f7806273206f5378eff6a4087c4f4922fa3a
NikRuchkin1 <prolive419000@gmail.com>
2020-11-09 01:15:54 +0300

    Change task project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './scss/style.scss'
-import {Home} from './pages'
 
 function App() {
   return (
     <div className="App">
-      <Home />
     </div>
   );
 }
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header';
-export { default as AllUsers } from './AllUsers';
-export { default as Albums } from './Albums';
